1e6c7d1f5b2b4192ee1757428cd56db2a558e99b
DELL <DELL@DESKTOP-2FNEKMR>
2024-08-15 14:51:06 +0530

    commit 2

---
+++ src/script/alphabets.java
@@ -0,0 +1,17 @@
+package script;
+
+public class alphabets {
+
+		
+
+		public static void main(String[] args)
+		{
+			for(char value='A';value<'Z';value++)
+			{
+				System.out.print(value);
+			}
+
+		}
+
+
+}
---
+++ src/script/fibonacci.java
@@ -0,0 +1,21 @@
+package script;
+
+
+	public class fibonacci {
+public static void main(String[] args)
+	
+		{
+			int fib1=0;
+			int fib2=1;
+			int fib3;
+			for(int i=1;i<=10;i++)
+				System.out.println(fib1+" "+fib2);	
+			{
+				fib3=fib1+fib2;
+				System.out.println(" "+fib3);
+				fib1=fib2;
+				fib2=fib3;
+			}
+	}
+
+}
