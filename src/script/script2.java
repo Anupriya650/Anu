366b5cea54b72717c714c154ea3866fcb11de604
DELL <DELL@DESKTOP-2FNEKMR>
2024-08-15 14:58:23 +0530

    commit45

---
+++ src/script/script2.java
@@ -0,0 +1,21 @@
+package script;
+
+import org.testng.annotations.Test;
+
+public class script2 {
+	public class reverse 
+	{
+	@Test
+	public void rev()
+	{
+		String n="anu";
+		String rev=" ";
+		for(int i=n.length()-1;i>=0;i--)
+		{
+			rev =rev+n.charAt(i);
+	}
+	System.out.println(rev);
+	}
+	}
+
+}
